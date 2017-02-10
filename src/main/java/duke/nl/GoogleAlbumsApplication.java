9bb4f93cd7dcadbac30eed672c040deaba17d08e
Bart Kors <bart@dukesolutions.nl>
2017-02-10 22:34:51 +0100

    added docker

--- src/main/java/duke/nl/GoogleAlbumsApplication.java
+++ src/main/java/duke/nl/GoogleAlbumsApplication.java
@@ -3,9 +3,7 @@ package duke.nl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,15 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-public class GoogleAlbumsApplication extends SpringBootServletInitializer {
+public class GoogleAlbumsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoogleAlbumsApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GoogleAlbumsApplication.class);
     }
 
 
